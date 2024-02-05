941777c9f4b6b7757f2affd1f8d9234e68583c97
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-05 22:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.getElementById("incrementBtn");
+let count=0;
+btn.addEventListener("click",()=>{
+	let ptagValue = document.getElementById("counter").innerText;
+	alert(ptagValue);
+	count+=1;
+	document.getElementById("counter").innerHTML = count
+})
